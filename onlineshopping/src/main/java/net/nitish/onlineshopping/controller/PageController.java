a8fb311896a0e06b768cd7fdb8a87aedbc3a5b53
Nitish Shrestha <nitishrestha8848@gmail.com>
2017-06-26 08:03:08 +0545

    completed the interface, added custom theme, footer problem also solved

--- onlineshopping/src/main/java/net/nitish/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/nitish/onlineshopping/controller/PageController.java
@@ -1,20 +1,39 @@
 package net.nitish.onlineshopping.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class PageController
-{
-	@RequestMapping(value={"/","/home","/index"})
-	public ModelAndView index()
-	{
-			ModelAndView mav = new ModelAndView("page");
-			
-			mav.addObject("message","Welcome to Online Shopping pvt. ltd.");
-		
-			return mav;
+public class PageController {
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView("page");
+
+		mav.addObject("userClickHome", true);
+		mav.addObject("title", "Home");
+
+		return mav;
 	}
+	
+	@RequestMapping(value ="/about")
+	public ModelAndView about() {
+		ModelAndView mav = new ModelAndView("page");
+
+		mav.addObject("userClickAbout", true);
+		mav.addObject("title", "About Us");
+
+		return mav;
+	}
+	
+	@RequestMapping(value ="/contact")
+	public ModelAndView contact() {
+		ModelAndView mav = new ModelAndView("page");
+
+		mav.addObject("userClickContact", true);
+		mav.addObject("title", "Contact Us");
+
+		return mav;
+	}
+	
 }
